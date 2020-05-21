4450ffbddf7fdd1c9627ead027802f0cdbc09005
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-05-21 22:08:44 +0200

    Add ColorHelpers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
-
+import {generatePalete} from './colorHelpers';
 class App extends Component {
   render() {
     return (
