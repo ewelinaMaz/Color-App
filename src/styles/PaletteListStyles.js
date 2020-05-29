73753a366ad3f9ba5d18df11984f2ca1df42a4a7
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-05-29 17:43:46 +0200

    Add NewPaletteForm and Link

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+
 export default {
     root: {
         backgroundColor: "blue",
@@ -17,7 +18,12 @@ export default {
         display: "flex",
         width: "100%",
         justifyContent: "space-between",
-        color: "white"
+        alignItems: "center",
+        color: "white",
+        "& a": {
+        textDecoration: "none",
+        color: "white",
+        }
     },
     palettes: {
         boxSizing: "border-box",
