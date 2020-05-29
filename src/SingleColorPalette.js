9520d3051c3ec43018d929d18dd350f7a040b074
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-05-29 13:57:34 +0200

    Rafacotor Navbar styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,7 +4,7 @@ import Nav from './Navbar';
 import Footer from './Footer';
 import {Link} from 'react-router-dom';
 import { withStyles } from "@material-ui/styles";
-import styles from "./styles/PaletteStyles";
+import styles from './styles/PaletteStyles';
 
 class SingleColorPalette extends Component {
     constructor(props) {
@@ -42,7 +42,7 @@ class SingleColorPalette extends Component {
             />
         ));
         return (
-            <div className={classes.palette}>
+            <div className={classes.Palette}>
                 <Nav handleChange={this.changeFormat}
                     showingAllColors={false} />
                 <div className={classes.colors}>
