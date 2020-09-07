046e57d171866b12ba54d3f881c422b0af64b76b
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-09-07 21:22:34 +0200

    make form and navbar responsive

--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -1,42 +1,51 @@
 import { DRAWER_WIDTH } from "../constants";
+import sizes from "./sizes";
+
 const drawerWidth = DRAWER_WIDTH;
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    display: "flex"
+    display: "flex",
   },
   hide: {
-    display: "none"
+    display: "none",
   },
   appBar: {
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
+      duration: theme.transitions.duration.leavingScreen,
     }),
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    height: "64px"
+    height: "64px",
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
     marginLeft: 12,
-    marginRight: 20
+    marginRight: 20,
   },
   navBtns: {
     marginRight: "1rem",
     "& a": {
       textDecoration: "none"
-    }
+    },
+    [sizes.down("xs")]: {
+      marginRight: "0.5rem"
+    },
   },
   button: {
-    margin: "0 0.5rem"
+    margin: "0 0.5rem",
+    [sizes.down("xs")]: {
+      margin: "0 0.2rem",
+      padding: "0.3rem",
+    }
   }
 });
 
