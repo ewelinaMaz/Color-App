268a6fd9aa2c3de8fe15e674984e9622dc481c9f
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-09-07 12:26:22 +0200

    Add media queries helper for color box styles

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,35 +1,48 @@
-
+import sizes from "./sizes";
 export default {
-    root: {
-        backgroundColor: "blue",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%",
     },
-    container: {
-        width: "50%",
-        display: "flex",
-        alignItems: "flex-start",
-        flexDirection: "column",
-        flexWrap: "wrap"
+    [sizes.down("xs")]: {
+      width: "75%",
     },
-    nav: {
-        display: "flex",
-        width: "100%",
-        justifyContent: "space-between",
-        alignItems: "center",
-        color: "white",
-        "& a": {
-        textDecoration: "none",
-        color: "white",
-        }
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "white",
+    "& a": {
+      textDecoration: "none",
+      color: "white",
     },
-    palettes: {
-        boxSizing: "border-box",
-        width: "100%",
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%"
-    }
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1rem",
+    },
+  },
 };
