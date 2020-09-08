b17932c6f6ec56a7a601542d0e89e18ae823a524
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-09-08 20:52:31 +0200

    animate palette deletion

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,39 +1,44 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import MiniPalette from './MiniPalette';
-import { withStyles } from '@material-ui/styles';
-import styles from './styles/PaletteListStyles';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { withStyles } from "@material-ui/styles";
+import styles from "./styles/PaletteListStyles";
 
 class PaletteList extends Component {
-    goToPalette(id) {
-        this.props.history.push(`/palette/${id}`);
-      }
-    render() {
-        const { palettes, classes, deletePalette } = this.props;
-        return (
-            <div className={classes.root}>
-                <div className={classes.container}>
-                    <nav className={classes.nav}>
-                        <h1 className={classes.Title}>
-                            React Colors
-                        </h1>
-                        <Link to='/palette/new'>Create Palette</Link>
-                    </nav>
-                    <div className={classes.palettes}>
-                        {palettes.map(palette => (
-                            <MiniPalette {...palette} 
-                            handleClick={() => this.goToPalette(palette.id)}
-                            handleDelete={deletePalette}
-                            key={palette.id}
-                            id={palette.id}
-                            />
-                        ))}
-                    </div>
-                </div>
-
-
-            </div>
-        );
-    }
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
+  }
+  render() {
+    const { palettes, classes, deletePalette } = this.props;
+    return (
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1 className={classes.Title}>React Colors</h1>
+            <Link to="/palette/new">Create Palette</Link>
+          </nav>
+          <div >
+            <TransitionGroup className={classes.palettes}>
+              {palettes.map((palette) => (
+                <CSSTransition
+                key={palette.id}
+                classNames="fade"
+                timeout={500}>
+                  <MiniPalette
+                    {...palette}
+                    handleClick={() => this.goToPalette(palette.id)}
+                    handleDelete={deletePalette}
+                    key={palette.id}
+                    id={palette.id}
+                  />
+                </CSSTransition>
+              ))}
+            </TransitionGroup>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default withStyles(styles)(PaletteList);
